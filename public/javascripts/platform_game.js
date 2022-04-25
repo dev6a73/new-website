fef8fb0bb9b7f37bde7f72b9d49e5ec5dab3c6be
dev6a73 <dev6a73@gmail.com>
2022-04-25 23:51:45 +0900

    Update platform_game.js

--- public/javascripts/platform_game.js
+++ public/javascripts/platform_game.js
@@ -139,7 +139,6 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 
-// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
 level = params.level
 
 Composite.add(world, [
