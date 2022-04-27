66c5f5ef38686a4b4314e2b7f92bb803621c2590
dev6a73 <dev6a73@gmail.com>
2022-04-27 16:30:26 +0900

    Update platform_game.js

--- public/javascripts/platform_game.js
+++ public/javascripts/platform_game.js
@@ -23,7 +23,7 @@ var maxSpeed = 1
 var mousefocus = false
 var π = Math.PI
 var fullscreen = true
-var level = 0
+var level = "main"
 var totalDeaths = 0
 
 var {
@@ -83,63 +83,93 @@ var player = {
     ]
 }
 player.bodies[2] = Example.ragdoll.ragdoll(200, 300, 0.5)
-var map = [Composites.stack(), Composites.stack(), Composites.stack(), Composites.stack(), Composites.stack(), Composites.stack]
-Composite.add(map[0], [
-    Bodies.rectangle(100, 500, 400, 200, { isStatic: true, }),
-    Bodies.rectangle(700, 500, 400, 400, { isStatic: true, }),
-    Bodies.rectangle(400, 350, 100, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
-    Bodies.rectangle(750, 250, 25, 25, { isStatic: true, render: { fillStyle: '#7E886A' }, id: 'moveto: 1' }),
-    Bodies.rectangle(-300, 300, 10, 800, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
-    Bodies.rectangle(1100, 300, 10, 800, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
-    Bodies.rectangle(400, -100, 1400, 10, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
-    Bodies.rectangle(400, 700, 1400, 10, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
-]); map[0].settings = { startPos: { x: 200, y: 300 } };
-Composite.add(map[1], [
-    Bodies.rectangle(300, 500, 50, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
-    Bodies.rectangle(400, 470, 50, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
-    Bodies.rectangle(200, 300, 50, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
-    Bodies.rectangle(450, 270, 50, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
-    Bodies.rectangle(550, 160, 75, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
-    Bodies.rectangle(550, 150, 50, 10, { isStatic: true, render: { fillStyle: '#7E886A' }, id: 'moveto: 2' }),
-    Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-    Bodies.rectangle(400, -50, 800, 50, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
-    Bodies.rectangle(800, 275, 50, 700, { isStatic: true }),
-    Bodies.rectangle(0, 275, 50, 700, { isStatic: true }),
-]); map[1].settings = { startPos: { x: 200, y: 500 } };
-Composite.add(map[2], [
-    Bodies.rectangle(300, 300, 10, 550, { isStatic: false, render: { fillStyle: '#ececd1' }, collisionFilter: { group: -2 } }),
-    Bodies.rectangle(300, 300, 10, 150, { isStatic: false, render: { fillStyle: '#f55a3c' }, collisionFilter: { group: -2 } }),
-    Bodies.rectangle(400, 300, 50, 50, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
-    Bodies.rectangle(650, 575, 300, 10, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
-    Bodies.rectangle(700, 400, 25, 25, { isStatic: true, render: { fillStyle: '#7E886A' }, id: 'moveto: 3' }),
-    Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-    Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-    Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
-]);
-Composite.add(map[2], [
-    Constraint.create({
-        bodyA: map[2].bodies[0],
-        pointA: { x: 0, y: 0 },
-        bodyB: map[2].bodies[1],
-        pointB: { x: 0, y: 0 },
-        stiffness: 1,
-        render: {
-            lineWidth: 1,
-            strokeStyle: '#f55a3c',
-        }
-    })
-])
-map[2].settings = { startPos: { x: 150, y: 500 } };
-Composite.add(map[3], [
-    Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-]); map[3].settings = { startPos: { x: 150, y: 500 } };
+var map = {}
+var load = {
+    "main": function () {
+        this["main"] = () => {} 
+        map["main"] = Composites.stack()
+        Composite.add(map["main"], [
+            Bodies.rectangle(400, 400, 100, 50, { isStatic: true, render: { fillStyle: '#7E886A'}, id: 'start'}),
+        ])
+        map["main"].settings = { startPos: { x: null, y: null } };
+    },
+    "0": () => {
+        load[0] = () => {}
+        map[0] = Composites.stack()
+        Composite.add(map[0], [
+            Bodies.rectangle(100, 500, 400, 200, { isStatic: true, }),
+            Bodies.rectangle(700, 500, 400, 400, { isStatic: true, }),
+            Bodies.rectangle(400, 350, 100, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
+            Bodies.rectangle(750, 250, 25, 25, { isStatic: true, render: { fillStyle: '#7E886A' }, id: 'moveto: 1' }),
+            Bodies.rectangle(-300, 300, 10, 800, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
+            Bodies.rectangle(1100, 300, 10, 800, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
+            Bodies.rectangle(400, -100, 1400, 10, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
+            Bodies.rectangle(400, 700, 1400, 10, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
+        ])
+        map[0].settings = { startPos: { x: 200, y: 300 } };
+    },
+    "1": () => {
+        map[1] = Composites.stack()
+        Composite.add(map[1], [
+            Bodies.rectangle(300, 500, 50, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
+            Bodies.rectangle(400, 470, 50, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
+            Bodies.rectangle(200, 300, 50, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
+            Bodies.rectangle(450, 270, 50, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
+            Bodies.rectangle(550, 160, 75, 10, { isStatic: true, render: { fillStyle: '#ececd1' } }),
+            Bodies.rectangle(550, 150, 50, 10, { isStatic: true, render: { fillStyle: '#7E886A' }, id: 'moveto: 2' }),
+            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+            Bodies.rectangle(400, -50, 800, 50, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
+            Bodies.rectangle(800, 275, 50, 700, { isStatic: true }),
+            Bodies.rectangle(0, 275, 50, 700, { isStatic: true }),
+        ]);
+        map[1].settings = { startPos: { x: 200, y: 500 } };
+    },
+    "2": () => {
+        map[2] = Composites.stack()
+        Composite.add(map[2], [
+            Bodies.rectangle(300, 300, 10, 550, { isStatic: false, render: { fillStyle: '#ececd1' }, collisionFilter: { group: -2 } }),
+            Bodies.rectangle(300, 300, 10, 150, { isStatic: false, render: { fillStyle: '#f55a3c' }, collisionFilter: { group: -2 } }),
+            Bodies.rectangle(400, 300, 50, 50, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
+            Bodies.rectangle(650, 575, 300, 10, { isStatic: true, render: { fillStyle: '#f55a3c' } }),
+            Bodies.rectangle(700, 400, 25, 25, { isStatic: true, render: { fillStyle: '#7E886A' }, id: 'moveto: 3' }),
+            Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+            Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+            Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
+        ]);
+        Composite.add(map[2], [
+            Constraint.create({
+                bodyA: map[2].bodies[0],
+                pointA: { x: 0, y: 0 },
+                bodyB: map[2].bodies[1],
+                pointB: { x: 0, y: 0 },
+                stiffness: 1,
+                render: {
+                    lineWidth: 1,
+                    strokeStyle: '#f55a3c',
+                }
+            })
+        ])
+        map[2].settings = { startPos: { x: 150, y: 500 } };
+    },
+    "3": () => {
+        map[3] = Composites.stack()
+        Composite.add(map[3], [
+            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+        ]);
+        map[3].settings = { startPos: { x: 150, y: 500 } };
+    }
+}
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 
-level = params.level || 0
+level = params.level || "main"
+newLevel(level)
+
+load["main"]()
+Composite.remove(world, map[level])
 
 Composite.add(world, [
     // walls
@@ -183,14 +213,7 @@ Events.on(engine, 'collisionStart', function (event) {
         }
         if ((3 <= pair.bodyA.id && pair.bodyA.id <= 12) || (3 <= pair.bodyB.id && pair.bodyB.id <= 12)) {
             if (pair.bodyA.id.toString().match('moveto:') || pair.bodyB.id.toString().match('moveto:')) {
-                Composite.remove(world, map[level])
-                level = parseInt(pair.bodyA.id.toString().match('moveto:') ? pair.bodyA.id.toString()[8] : pair.bodyB.id.toString()[8])
-                if (history.pushState) {
-                    var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?level=' + level;
-                    window.history.pushState({ path: newurl }, '', newurl);
-                }
-                player.bodies[2].bodies.forEach(i => Body.setPosition(i, map[level].settings.startPos))
-                Composite.add(world, map[level])
+                newLevel(parseInt(pair.bodyA.id.toString().match('moveto:') ? pair.bodyA.id.toString()[8] + pair.bodyA.id.toString()[9] : pair.bodyB.id.toString()[8] + pair.bodyB.id.toString()[9], 16))
             }
             if (pair.bodyA.render.fillStyle == '#f55a3c' || pair.bodyB.render.fillStyle == '#f55a3c') {
                 //player.bodies[2].bodies.forEach(i => Body.setPosition(i, map[level].settings.startPos))
@@ -217,7 +240,9 @@ Events.on(engine, 'collisionEnd', function (event) {
 
 function inGame() {
     ctx.clearRect(0, 0, c.width, c.height)
-
+    if(mouseConstraint.body?.id == 'start'){
+        newLevel(0)
+    }
     balance(1.1)
 
     if (player.bodies[0].velocity.x > 20) {
@@ -244,6 +269,21 @@ function inGame() {
     }
 }
 
+function newLevel(num) {
+    try {
+        Composite.remove(world, map[level])
+    } catch (error) {
+        console.log(error)
+    }
+    level = isNaN('0x' + num) ? num : parseInt(num, 16)
+    load[level]()
+    if (history.pushState) {
+        var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?level=' + level;
+        window.history.pushState({ path: newurl }, '', newurl);
+    }
+    player.bodies[2].bodies.forEach(i => Body.setPosition(i, map[level].settings.startPos))
+    Composite.add(world, map[level])
+}
 function changeMode(e) {
     editMode = e
     if (e == -1) {
@@ -279,14 +319,6 @@ function arrowKeyMove(event) {
 }
 function eventHandler() {
     document.getElementById("canvas1").addEventListener("pointermove", (event) => { pointerxy[0] = event.x - c.offsetLeft; pointerxy[1] = event.y - c.offsetTop; })
-    var changemode = document.getElementsByClassName("changemode");
-    changemode[0].addEventListener("click", () => { changeMode("playing") })
-    changemode[1].addEventListener("click", () => { changeMode(0) })
-    changemode[2].addEventListener("click", () => { changeMode(1) })
-    changemode[3].addEventListener("click", () => { changeMode(2) })
-    changemode[4].addEventListener("click", () => { changeMode(3) })
-    changemode[5].addEventListener("click", () => { changeMode(4) })
-
     document.addEventListener("keypress", (event) => { arrowKeyMove(event) })
     document.getElementById("advancedopener").addEventListener("click", () => { document.getElementById('Advanced').style.display = 'block' })
     document.getElementById("applyStartPos").addEventListener("click", () => { moveStartPos() })
